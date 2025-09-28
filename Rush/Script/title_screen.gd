9e10382c22d6ec6b595de79177c5dddfb5068d2b
Patty <94728986+PattyHoswell@users.noreply.github.com>
2025-09-28 19:46:26 +0700

    Deleted Credits Button from Menu

--- Rush/Script/title_screen.gd
+++ Rush/Script/title_screen.gd
@@ -3,13 +3,9 @@ extends Node2D
 func _ready() -> void:
 	%"Start Game".pressed.connect(_on_start_game_pressed)
 	%"Options".pressed.connect(_on_options_pressed)
-	%"Credits".pressed.connect(_on_credits_pressed)
 
 func _on_start_game_pressed() -> void:
 	print("Pressed Start")
 
 func _on_options_pressed() -> void:
 	%OptionsMenu.set_menu_visible(true)
-
-func _on_credits_pressed() -> void :
-	print("Pressed Credits")
